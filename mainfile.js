b661562b7c4f08221fdf13ba51d5b8d8e074bd9c
emmeiwhite <emmeiwhite@gmail.com>
2018-09-27 16:14:16 +0530

    Added payload in ACTIONS, State will updaet based on payload now

--- mainfile.js
+++ mainfile.js
@@ -12,15 +12,21 @@ const reducer = (state=initialState,action)=>{
 
        const newState =  {...state};// ES6 Destructuring Concept
 
-       newState.balance+=2000;
+       newState.balance+=action.payload;
        return newState; // Reducer is updating the state in this case.As if we are calling setState({}) in react
     }
 
       
     if(action.type==='DECR'){
        const newState =  {...state};
-       newState.balance-=2000;
+       newState.balance-=action.payload;
        return newState; 
+    }
+
+    if(action.type==='BONUS'){
+        const newState = {...state};
+        newState.balance+=action.payload;
+        return newState;
     }
 
     return state;
@@ -39,23 +45,45 @@ store.subscribe(()=>{
 // Since we aren't using any UI in this redux application we will directly dispatch our actions from here only
 
 // console.log(store.getState());
-store.dispatch({type:'INCR'});
+store.dispatch(
+    {
+        type:'INCR',
+        payload:4000
+    });
 
 //  Let's use one more action DECR to decrement the amout
 
 // console.log(store.getState());
-store.dispatch({type:'DECR'});
+store.dispatch(
+    {
+        type:'DECR',
+        payload:2000
+    });
+
+store.dispatch(
+    {
+        type:'BONUS',
+        payload:5000
+    }
+)
 
 
 // Let's dispatch action which is not known to the reducer
 
 // console.log(store.getState());
-store.dispatch({type:'DECR'});
-store.dispatch({type:'DECR'});
-store.dispatch({type:'DECR'});
-store.dispatch({type:'DECR'});
+store.dispatch(
+    {
+        type:'DECR',
+        payload:2000
 
-store.dispatch({type:'BONUS'});
+    });
+
+
+store.dispatch(
+    {
+        type:'BONUS',
+        payload:5000
+    });
 
 // console.log(store.getState())
 
@@ -66,6 +94,11 @@ store.dispatch({type:'BONUS'});
 
  Let's just do that: I have commented all the previous logs. Just after the store is created we have used our subscription ^^^
 
+*/
+
+/*
+    LASTLY, I will make use of the payload and Increment, decrement amount based on the payload in Reducers.
+    This will probably be the last commit of this application. i will InshaALLAH go with new learning stuff and upload to github
 */
 
 
